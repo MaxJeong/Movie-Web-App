f571d1a2781f6b78fad62c5a8171eb4dc1f45ff4
wuaddiso <addison.wu@mail.utoronto.ca>
2015-10-09 16:25:28 -0400

    basic event handler created,minor style change in details

--- public/js/views/details.js
+++ public/js/views/details.js
@@ -7,6 +7,34 @@ var splat = splat || {};
 // note View-name (Home) matches name of template file Home.html
 splat.Details = Backbone.View.extend({
 
+	events:{
+		//add event to check if user leaves without saving
+		"click #moviesave ": "save",
+		"click #moviedel ": "delete"
+
+	},
+
+	//updates the model in the collection, if there isn't one, create it
+	save:function(event){
+		// console.log('trying to save');
+		// console.log(splat.collection);
+		
+
+		$("input[type*='text']").each(function(item){
+			console.log($(this).attr('name'));
+			// console.log(item.attr('name'));
+		});
+	},
+
+	//delete model in collection, prompt user to double check,
+	//return to browse view when done
+	delete:function(event){
+
+	},
+
+	//validator, validates fields,may require multiple functions,
+
+
     // render the View
     render: function () {
 	// set the view element ($el) HTML content using its template
